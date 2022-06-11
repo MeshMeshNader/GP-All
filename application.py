5f1eefb6461f945c0e3827c3bb318f7a05a10cb6
MeshMeshNader <meshmeshnader555@gmail.com>
2022-06-11 16:45:10 +0200

    Initial Submit

--- application.py
+++ application.py
@@ -3,15 +3,19 @@ import re
 import os
 import six
 import base64
-import logging
+import face_recognition
+import numpy as np
+import urllib.request
+import json
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from msrest.authentication import ApiKeyCredentials
-from flask import Flask, request, abort
 from google.cloud import vision
 from google.cloud import translate_v2 as translate
+from flask import Flask, request, abort
+import logging
 
 app = Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
@@ -25,6 +29,9 @@ ENDPOINT_FACE = 'https://myfaceapi2.cognitiveservices.azure.com/'
 face_client = FaceClient(ENDPOINT_FACE, CognitiveServicesCredentials(API_KEY_FACE))
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+
+TOLERANCE = 0.59
+MODEL = 'cnn'  # 'hog' or 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
 
 
 def ImageCaptioning(img):
@@ -209,7 +216,7 @@ def Currency_Recognition(img):
         return "There was an Error while Detecting"
 
 
-@app.route("/AI", methods=['POST'])
+@app.route("/VisuallyImpaired", methods=['POST'])
 def AI():
     # print(request.json)
     if not request.json or 'image' not in request.json:
@@ -247,14 +254,85 @@ def AI():
     return result_dict
 
 
-@app.route("/TR", methods=['POST'])
-def TR():
+@app.route("/Translation", methods=['POST'])
+def Translation():
     if not request.json or 'text' not in request.json:
         abort(400)
 
     text = request.json['text']
 
     result = translate_text(text, 'ar')
+
+    result_dict = {"output": result}
+    return result_dict
+
+
+@app.route("/FaceRecognitionTraining", methods=['POST'])
+def Training_Faces():
+    if not request.json or 'urls' not in request.json:
+        abort(400)
+
+    all_urls = json.loads(request.json['urls'])
+
+    known_names = []
+    known_faces = []
+
+    for key, value in all_urls.items():
+        response = urllib.request.urlopen(value)
+        image = face_recognition.load_image_file(response)
+
+        encoding = face_recognition.face_encodings(image)[0]
+
+        known_faces.append(encoding)
+        known_names.append(key)
+
+    data = {}
+    for key in known_names:
+        for value in known_faces:
+            data[key] = value.tolist()
+            known_faces.remove(value)
+            break
+
+    new_data = json.dumps(data)
+    result_dict = {"output": new_data}
+    return result_dict
+
+
+@app.route("/FaceRecognitionTesting", methods=['POST'])
+def Recognize_Face():
+    if not request.json or 'encodings' not in request.json:
+        abort(400)
+
+    if not request.json or 'url' not in request.json:
+        abort(400)
+
+    all_face_encodings = json.loads(request.json['encodings'])
+
+    img_url = request.json['url']
+
+    known_names = list(all_face_encodings.keys())
+    known_faces = np.array(list(all_face_encodings.values()))
+
+    response = urllib.request.urlopen(img_url)
+    image = face_recognition.load_image_file(response)
+
+    locations = face_recognition.face_locations(image, model=MODEL)
+
+    encodings = face_recognition.face_encodings(image, locations)
+
+    for face_encoding, face_location in zip(encodings, locations):
+
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+        match = None
+
+        if True in results:
+            match = known_names[results.index(True)]
+            result = "I Found " + match
+
+        check = all(element == False for element in results)
+
+        if check:
+            result = "I Found Unknown Person"
 
     result_dict = {"output": result}
     return result_dict
